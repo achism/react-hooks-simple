d64eb734c0b714ac4d07736df410b0f6d6a63cdd
Alex Chism <37649529+ASChism@users.noreply.github.com>
2019-06-29 20:06:11 -0700

    Invoke arrow function directly inside useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 
 const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async (resource) => {
-        const response = await Axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
-        setResources(response.data);
-    }
-
-    useEffect(() => { fetchResource(resource) }, [resource]);
+    useEffect(() => {
+        (async resource => {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+    
+            setResources(response.data);
+        })(resource);
+    }, [resource]);
     
     return (
         <div>
