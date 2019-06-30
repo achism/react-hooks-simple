21d80ac7088e2511324a900a526055b7cae4c7c8
Alex Chism <37649529+ASChism@users.noreply.github.com>
2019-06-29 19:30:20 -0700

    Add lifecycle methods to ResourceList

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Axios from 'axios';
 
 class ResourceList extends React.Component {
+    state = { resources: [] };
+
+    async componentDidMount() {
+        const response = await Axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+        this.setState({ resources: response.data });
+    }
+
+    async componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            const response = await Axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+            this.setState({ resources: response.data });
+        }
+    }
+
     render() {
         return (
             <div>
-                {this.props.resource}
+                {this.state.resources.length}
             </div>
         );
     }
